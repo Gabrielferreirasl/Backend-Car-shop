b763a9b68daf7b51d8be3c8cab39186b32d8822b
Gabriel <gabrielferreiradsl@hotmail.com>
2022-04-22 17:05:38 -0300

    feat: "Model" interface added

---
+++ src/interfaces/ModelInterface.ts
@@ -0,0 +1,7 @@
+export interface Model<T> {
+  create(obj: T): Promise<T>
+  read(): Promise<T[]>
+  readOne(string: string): Promise<T | null>
+  update(string: string, obj: T): Promise<T | null>
+  delete(string: string): Promise<T | null>
+}
