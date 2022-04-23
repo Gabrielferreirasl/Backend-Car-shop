784794b532cf658a043dd01264c78ab568fb47a2
Gabriel <gabrielferreiradsl@hotmail.com>
2022-04-23 18:20:50 -0300

    fix: CarInterface, VehicleInterface, zod implemented

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,6 +1,12 @@
-import { Vehicle } from './VehicleInterface';
+import { z } from 'zod';
+import schemaVehicle from './VehicleInterface';
 
-export interface Car extends Vehicle {
-  doorsQty: number,
-  seatsQty: number
-}
+const schemaCar = z.object({
+  ...schemaVehicle.shape,
+  doorsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
+});
+
+export default schemaCar;
+
+export type Car = z.infer<typeof schemaCar>;
--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,7 +1,13 @@
-export interface Vehicle {
-  model: string,
-  year: number,
-  color: string,
-  status?: boolean,
-  buyValue: number,
-}
+import { z } from 'zod';
+
+const schemaVehicle = z.object({
+  model: z.string().min(3).nonempty(),
+  year: z.number().min(1900).max(2022),
+  color: z.string().min(3).nonempty(),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export default schemaVehicle;
+
+export type Vehicle = z.infer<typeof schemaVehicle>;
