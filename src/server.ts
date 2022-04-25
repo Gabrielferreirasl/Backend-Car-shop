6129b3c8f205d356ca6978eb5b089656f2699cfc
Gabriel <gabrielferreiradsl@hotmail.com>
2022-04-24 22:50:58 -0300

    db url changed

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
-// import CustomRouter from './routes/Router';
 import App from './app';
 import carsRouter from './routers/CarsRouter';
 import motorcycleRouter from './routers/MotorcycleRouter';
 
-// import exampleController from './controllers/controller-example';
-
-// import { example } from './interfaces/ExampleInterface';
-
 const server = new App();
-
-// exampleRouter.addRoute(exampleController);
 
 server.addRouter(carsRouter);
 server.addRouter(motorcycleRouter);
